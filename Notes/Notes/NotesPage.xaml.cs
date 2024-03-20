dd7f0c64b1012a67054e7494ac24818d7fafb0ff
mt020308 <mt02032008@hotmail.com>
2024-03-20 18:09:15 -0300

    feat Adicionei a página Anotações

---
+++ Notes/Notes/NotesPage.xaml.cs
@@ -0,0 +1,28 @@
+namespace Notes;
+
+public partial class NotesPage : ContentPage
+{
+    string path = Path.Combine(FileSystem.AppDataDirectory, "file.txt");
+    string text = "";
+
+	public NotesPage()
+	{
+		InitializeComponent();
+        if (File.Exists(path))
+        {
+            FileEditor.Text = File.ReadAllText(path);
+        }
+        
+	}
+
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        text = FileEditor.Text;
+        File.WriteAllText(path, text);
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        File.Delete(path);
+    }
+}
